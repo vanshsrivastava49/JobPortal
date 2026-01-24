01b51a2cd1419d8997c6c6a088dc94a2fff8fc48
vanshsrivastava49 <srivastavavansh11@gmail.com>
2026-01-24 14:28:47 +0530

    user roles defined

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,34 +1,28 @@
 const mongoose = require("mongoose");
-
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, unique: true, required: true, lowercase: true },
     password: { type: String, required: true },
-
     role: {
       type: String,
       enum: ["jobseeker", "recruiter", "business", "admin"],
       required: true
     },
-
     companyId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Company",
       default: null
     },
-
     status: {
       type: String,
       enum: ["active", "blocked"],
       default: "active"
     },
-
     isVerified: {
       type: Boolean,
       default: false
     }
   },
-  { timestamps: true }
-);  
+  { timestamps: true });
 module.exports = mongoose.model("User", userSchema);
